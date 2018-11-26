d7a53fda8f2e4a603b340c7b17eb45a452705001
shoreviewanalytics <chad.downey@shoreviewanalytics.com>
2018-11-26 23:07:33 +0000

    more edits

--- src/main/java/com/java/spark/SparkLogBackExampleThree.java
+++ src/main/java/com/java/spark/SparkLogBackExampleThree.java
@@ -2,14 +2,22 @@ package com.java.spark;
 
 
 import org.apache.spark.api.java.JavaSparkContext;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FsStatus;
+import org.apache.hadoop.fs.Hdfs;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.permission.FsPermission;
 import org.apache.spark.SparkConf;
+import com.datastax.bdp.fs.hadoop.*;
 
 import org.slf4j.LoggerFactory;
 
@@ -20,9 +28,11 @@ import ch.qos.logback.core.joran.spi.JoranException;
 import ch.qos.logback.core.util.StatusPrinter;
 
 
+@SuppressWarnings("unused")
 public class SparkLogBackExampleThree {
 	
 	final static Logger logger = LoggerFactory.getLogger(SparkLogBackExampleThree.class);
+	private static final String rawData = null;
 	
 	public static void main(String[] args) {
 		
@@ -53,41 +63,80 @@ public class SparkLogBackExampleThree {
 			
 		    //FileSystem fileSystem = FileSystem.get(new URI("dsefs://10.1.10.51:5598"),hdfsconf);	
 			
-			FileSystem fileSystem = FileSystem.get(hdfsconf);
+			DseFileSystem fileSystem = (DseFileSystem) DseFileSystem.get(hdfsconf);
 			
-			Path home = fileSystem.getHomeDirectory();  // this is the home directory for user running the job. 
+			Path home = fileSystem.getHomeDirectory();  // home dir for user 
+			
+			
 						
 			logger.info(home.toString());
 						
-			//FSDataInputStream inputStream = fileSystem.open(new Path("dsefs://10.1.10.51:5598/jobs/sle3/logback.xml"));
+						
+			boolean exists = fileSystem.exists(new Path("dsefs://10.1.10.51:5598/jobs/sle3/logback.xml"));
 			
-			boolean exists = fileSystem.exists(new Path("file:///jobs/sle3/logback.xml"));
+			boolean isfile = fileSystem.isFile(new Path("dsefs://10.1.10.51:5598/jobs/sle3/logback.xml"));
 			
+			
+			// change permission  
+			//fileSystem.setPermission(new Path("file:///jobs/sle3/logback.xml"), new FsPermission((short)0777));
+			
+			// change owner
+			//fileSystem.setOwner(new Path("file:///jobs/sle3/logback.xml"), "one", "one");
+			
+					
 			if (exists == true) {
 
-				FSDataInputStream inputStream = fileSystem.open(new Path("file:///jobs/sle3/logback.xml"));
+				//FSDataInputStream inputStream = fileSystem.open(new Path("dsefs://10.1.10.51:5598/jobs/sle3/logback.xml"));
+				//InputStream inputStream = fileSystem.open(new Path("dsefs://10.1.10.51:5598/jobs/sle3/logback.xml"));
+				//InputStream inputStream = fileSystem.open(new Path("file:///jobs/sle3/logback.xml"));
 				
-				logger.info(inputStream.toString());
-
+				InputStream inputStream = fileSystem.open(new Path("dsefs://10.1.10.51:5598/jobs/sle3/logback.xml"));
+								
+							
+	/*			ObjectInputStream configStream = new ObjectInputStream(inputStream);*/
+				
+				BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+				
 				byte[] bs = new byte[inputStream.available()];
-				inputStream.readFully(bs);
-
-				ObjectInputStream configStream = new ObjectInputStream(inputStream);
-
-				try {
+				 
+				
+				/*try {
 
 					JoranConfigurator configurator = new JoranConfigurator();
 					configurator.setContext(context);
 					context.reset();
-					configurator.doConfigure(configStream);
+					configurator.doConfigure(inputStream);
 				} catch (JoranException je) {
 
 				}
 
-				StatusPrinter.printInCaseOfErrorsOrWarnings(context);
-
-			}
-
+				StatusPrinter.printInCaseOfErrorsOrWarnings(context);*/
+				
+				try {
+				
+					String line;
+					String text;
+					text = br.toString();
+					line = br.readLine();
+					
+					while (line != null) {
+						logger.info(line);
+					   
+						line = br.readLine();
+				    }
+				}	finally {
+						br.close();
+						inputStream.close();
+						fileSystem.close();
+						
+					}
+				
+				
+				
+				}
+				
+			
+			
 			// }catch(IOException | URISyntaxException | IllegalArgumentException e){
 		} catch (IOException | IllegalArgumentException e) {
 
@@ -96,6 +145,7 @@ public class SparkLogBackExampleThree {
 		}
 	    	
 		sc.close();
+		 
 
 	}
 
