4bf6f1ad0b0e2cd8cca6a058dfaef40d90286574
shoreviewanalytics <chad.downey@shoreviewanalytics.com>
2018-11-26 17:38:03 +0000

    added byos jar to path

--- src/main/java/com/java/spark/SparkLogBackExampleThree.java
+++ src/main/java/com/java/spark/SparkLogBackExampleThree.java
@@ -4,8 +4,6 @@ package com.java.spark;
 import org.apache.spark.api.java.JavaSparkContext;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
@@ -14,6 +12,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.spark.SparkConf;
 
 import org.slf4j.LoggerFactory;
+
 import org.slf4j.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
@@ -31,7 +30,7 @@ public class SparkLogBackExampleThree {
 		
 		SparkConf conf = new SparkConf()
 				.setAppName("SparkLogBackExampleThree")
-				.setMaster("local[1]") // comment out if not running locally.
+				.setMaster("local[1]") // comment when not running locally.
 				;
 				
 		
@@ -45,36 +44,55 @@ public class SparkLogBackExampleThree {
 			
 			Configuration hdfsconf = new Configuration();
 			
-			
+			hdfsconf.set("fs.defaultFS", "dsefs://10.1.10.51");
 			hdfsconf.set("fs.dsefs.impl", "com.datastax.bdp.fs.hadoop.DseFileSystem");
+			hdfsconf.set("com.datastax.bdp.fs.client.authentication.factory", "com.datastax.bdp.fs.hadoop.DseRestClientAuthProviderBuilderFactory");
 			hdfsconf.set("com.datastax.bdp.fs.client.authentication.basic.username", "cassandra");
 			hdfsconf.set("com.datastax.bdp.fs.client.authentication.basic.password", "");
-			hdfsconf.set("com.datastax.bdp.fs.client.authentication.factory", "com.datastax.bdp.fs.hadoop.RestClientAuthProviderBuilderFactory");
+			
+		    //FileSystem fileSystem = FileSystem.get(new URI("dsefs://10.1.10.51:5598"),hdfsconf);	
+			
+			FileSystem fileSystem = FileSystem.get(hdfsconf);
+			
+			Path home = fileSystem.getHomeDirectory();  // this is the home directory for user running the job. 
 						
-		    FileSystem fileSystem = FileSystem.get(new URI("dsefs://10.1.10.51:5598"),hdfsconf);		    
+			logger.info(home.toString());
+						
+			//FSDataInputStream inputStream = fileSystem.open(new Path("dsefs://10.1.10.51:5598/jobs/sle3/logback.xml"));
 			
-			FSDataInputStream inputStream = fileSystem.open(new Path("dsefs://10.1.10.51:5598/jobs/sle3/logback.xml"));
+			boolean exists = fileSystem.exists(new Path("file:///jobs/sle3/logback.xml"));
 			
-			ObjectInputStream configStream = new ObjectInputStream(inputStream);			
-			
-			try {
+			if (exists == true) {
+
+				FSDataInputStream inputStream = fileSystem.open(new Path("file:///jobs/sle3/logback.xml"));
 				
-			      JoranConfigurator configurator = new JoranConfigurator();
-			      configurator.setContext(context);
-			      context.reset(); 
-			      configurator.doConfigure(configStream);
-			    } catch (JoranException je) {
-			      
-			    }
-			
+				logger.info(inputStream.toString());
+
+				byte[] bs = new byte[inputStream.available()];
+				inputStream.readFully(bs);
+
+				ObjectInputStream configStream = new ObjectInputStream(inputStream);
+
+				try {
+
+					JoranConfigurator configurator = new JoranConfigurator();
+					configurator.setContext(context);
+					context.reset();
+					configurator.doConfigure(configStream);
+				} catch (JoranException je) {
+
+				}
+
 				StatusPrinter.printInCaseOfErrorsOrWarnings(context);
-				
-		}catch(IOException | URISyntaxException | IllegalArgumentException e){
-			
-			logger.error("An Error has occurred: " + e.getMessage());	
-			 
-			
-		}	
+
+			}
+
+			// }catch(IOException | URISyntaxException | IllegalArgumentException e){
+		} catch (IOException | IllegalArgumentException e) {
+
+			logger.error("An Error has occurred: " + e.getMessage());
+
+		}
 	    	
 		sc.close();
 
